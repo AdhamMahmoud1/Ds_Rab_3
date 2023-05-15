282185a20de8e4217dc2d847644a6b530bf2509e
Fady-K <popfadykamal151617@gmail.com>
2023-05-15 05:52:52 +0300

    added bst files

--- includes/bst.hpp
+++ includes/bst.hpp
@@ -1,29 +1,59 @@
+/**
+ * @file BST.hpp
+ * @author Fady Kamal (20210282)
+ * @brief Binary Search Tree Header file
+ * @version 0.1
+ * @date 2023-05-13
+ * 
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma once
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
 #include "Bnode.hpp"
 using namespace std;
-template <typename T>
-class BST {
-protected:
-    Bnode<T>  * root;
 
-    Bnode<T> *insert(Bnode<T> *r, T element);
-    Bnode<T> *search(Bnode<T> *r, T element);
-    Bnode<T> *Delete(Bnode<T> *, T element);
-    void preOrder(Bnode<T> *r);
-    void inOrder(Bnode<T> *r);
-    void postOrder(Bnode<T> *r);
+
+template<class T>
+class BST
+{
+protected:
+    /* Instance Attributes */
+    Bnode<T>* root;
+    
 
 public:
-    BST();
-    Bnode<T> *get_root();
-    void insert(T element);
-    bool search(T element);
-    Bnode<T> *findMaxmum(Bnode<T> *r);
-    Bnode<T> *findMinimum(Bnode<T> *r);
-    void Delete(T element);
-    
+    /* Constructors And Destructor */
+    BST();                                  // Default constructor
+    ~BST();                                 // Destructor
+
+
+
+    /* Setters & Getters */
+    Bnode<T> *get_root() const;
+
+
+    /* Instance Methodss  */
+    Bnode<T>* insert(Bnode<T>* subTreeRoot, T item);
+    void insert(T item);
+
+    Bnode<T>* search(Bnode<T>* subTreeRoot, T key);
+    bool search(T key);
+
+    Bnode<T>* Delete(Bnode<T>* subTreeRoot, T item);
+    void Delete(T item);
+
+    Bnode<T>* findMaxmum(Bnode<T>* subTreeRoot);
+    Bnode<T>* findMinimum(Bnode<T>* subTreeRoot);
+
+    void preOrder(Bnode<T>* subTreeRoot);
     void preOrder();
+    
+    void inOrder(Bnode<T>* subTreeRoot);
     void inOrder();
+
+    void postOrder(Bnode<T>* subTreeRoot);
     void postOrder();
+
+
 };
--- src/bst.cpp
+++ src/bst.cpp
@@ -1,190 +1,400 @@
+/**
+ * @file BST.cpp
+ * @author Fady Kamal (20210282)
+ * @brief BinarySearchTree implementation file
+ * @version 0.1
+ * @date 2023-05-13
+ * 
+ * 
+ */
 #include "../includes/bst.hpp"
 
-template <typename T>
-BST<T>::BST() : root(nullptr) {}
 
-template <typename T>
-Bnode<T> *BST<T>::insert(Bnode<T> *r, T element) {
-    if (r == nullptr)
-    {
-        Bnode<T> *newNode = new Bnode<T>(element);
-        r = newNode;
-    }
-    else if (element < r->data)
-    {
-        r->left = insert(r->left, element);
-    }
-    else
-    {
-        r->right = insert(r->right, element);
-    }
-    return r;
-}
+////////////////////////////////////////////////// Constructors And Destructor ////////////////////////////////////
+/**
+ * @brief Construct a new BST<T>::BST object
+ * @details Default constructor
+ * @tparam T Type of contained data
+ */
+template<class T>
+BST<T>::BST(): root(NULL){}
 
-template <typename T>
-void BST<T>:: insert(T element){
-    root = insert(root, element);
-}
 
-template <typename T>
-Bnode<T> *BST<T>::get_root() { return root; }
+/**
+ * @brief Destroy the BST<T>::BST object
+ * 
+ * @tparam T Type of contained data
+ */
+template<class T>
+BST<T>::~BST(){}
 
-template <typename T>
-Bnode<T> *BST<T>::search(Bnode<T> *r, T element)
+
+
+////////////////////////////////////////////////// Setters  & Getters  ////////////////////////////////////
+/**
+ * @brief Return tree's root
+ * 
+ * @tparam T Type of contained data
+ * @return Bnode<T>* Root of tree
+ */
+template<class T>
+Bnode<T>* BST<T>::get_root() const
 {
-    if (r == nullptr)
-    {
-        return nullptr;
-    }
-    else if (r->data == element)
-    {
-        return r;
-    }
-    else if (element < r->data)
-    {
-        return search(r->left, element);
-    }
-    else
-    {
-        return search(r->right, element);
-    }
+    return this->root;
 }
 
-template <typename T>
-bool BST<T>::search(T element)
-{
-    Bnode<T> *result = search(root, element);
-    return (result == nullptr) ? false : true;
-}
 
-template <typename T>
-Bnode<T> *BST<T>::findMinimum(Bnode<T> *r)
-{
-    if (r == nullptr)
-    {
-        return nullptr;
-    }
-    else if (r->left == nullptr)
-    {
-        return r;
-    }
-    else
-    {
-        return findMinimum(r->left);
-    }
-}
 
-template <typename T>
-Bnode<T> *BST<T>::findMaxmum(Bnode<T> *r)
+////////////////////////////////////////////////// Instance Methods  ////////////////////////////////////
+/**
+ * @brief Take item and insert it into the tree
+ * 
+ * @tparam T Type of contained data
+ * @param subTreeRoot Root of subTree that insertion will start at
+ * @param item Item to insrt
+ * @return Bnode<T>* Inserted root
+ */
+template<class T>
+Bnode<T>* BST<T>::insert(Bnode<T>* subTreeRoot, T item)
 {
-    if (r == nullptr)
-    {
-        return nullptr;
-    }
-    if (r->right == nullptr)
-    {
-        return r;
-    }
-    else
-    {
-        return findMaxmum(r->right);
-    }
-}
-template <typename T>
-Bnode<T> *BST<T>::Delete(Bnode<T> *r, T element)
-{
-    if (r == nullptr)
-    {
-        return nullptr;
-    }
-    if (element < r->data)
-    {
-        r->left = Delete(r->left, element);
-    }
-    else if (element > r->data)
-    {
-        r->right = Delete(r->right, element);
-    }
-    else
-    {
-        if (r->left == nullptr && r->right == nullptr)
+        if ( subTreeRoot == NULL)
         {
-            r = nullptr;
+            /**
+             *  if tree is empty, 
+             *  create a new node with new item, 
+             *  set its left and right childs to null (thanks to constructor),
+             *  update root
+             */
+
+            // create a new node 
+            Bnode<T>* newNode = new Bnode<T>();
+            newNode->data = item;
+
+            // set newNode as root 
+            subTreeRoot = newNode;
         }
-        else if (r->left != nullptr && r->right == nullptr)
+        else if (item < subTreeRoot->data)
         {
-            r->data = r->left->data;
-            delete r->left;
-            r->left = nullptr;
-        }
-        else if (r->left == nullptr && r->right != nullptr)
-        {
-            r->data = r->right->data;
-            delete r->right;
-            r->right = nullptr;
+            /**
+             *  if item < data of subtree's root 
+             *  insert data in left child of current root
+             */
+            subTreeRoot->left = insert(subTreeRoot->left, item);
         }
         else
         {
-            Bnode<T> *max = findMaxmum(r->left);
-            r->data = max->data;
-            r->left = Delete(r->left, max->data);
+            /**
+             *  if item >= data of subtree's root 
+             *  insert data in right child of current root
+             */
+            subTreeRoot->right = insert(subTreeRoot->right, item);
         }
-    }
-    return r;
-}
-template <typename T>
-void BST<T>::Delete(T element)
-{
-    root = Delete(root, element);
-}
-template <typename T>
-void BST<T>::preOrder(Bnode<T> *r)
-{
-    if (r == nullptr)
-    {
-        return;
-    }
-    std::cout << r->data << " ";
-    preOrder(r->left);
-    preOrder(r->right);
+
+        // return subTreeRoot to update tree's root
+        return subTreeRoot;
 }
 
-template <typename T>
-void BST<T>::inOrder(Bnode<T> *r)
+
+/**
+ * @brief Overloading with tree's main root
+ * 
+ * @tparam T Type of contained data
+ * @param item Item to insert
+ */
+template<class T>
+void BST<T>::insert(T item)
 {
-    if (r == nullptr)
-    {
-        return;
-    }
-    inOrder(r->left);
-    std::cout << r->data << " ";
-    inOrder(r->right);
+    // insert item in tree 
+    root = insert(this->root, item);
 }
 
-template <typename T>
-void BST<T>::postOrder(Bnode<T> *r)
+
+/**
+ * @brief Takes key and checks if in tree or not
+ * 
+ * @tparam T 
+ * @param subTreeRoot 
+ * @param key 
+ * @return Bnode<T>* 
+ */
+template<class T>
+Bnode<T>* BST<T>::search(Bnode<T>* subTreeRoot, T key)
 {
-    if (r == nullptr)
+    if (subTreeRoot == NULL)
     {
-        return;
+        // return wihtout doing any thing
+        return NULL;
     }
-    postOrder(r->left);
-    postOrder(r->right);
-    std::cout << r->data << " ";
+    else if (key < subTreeRoot->data)
+    {
+        // keep searching
+        return search(subTreeRoot->left, key);
+    }
+    else if (key > subTreeRoot->data)
+    {
+        // keep searching till be found and return
+        return search(subTreeRoot->right, key);
+    }
+    else
+    {
+        // found, return this node
+        return subTreeRoot;
+    }
 }
-template <typename T>
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @param key 
+ * @return true 
+ * @return false 
+ */
+template <class T>
+bool BST<T>::search(T key)
+{
+    // search for item starting from root
+    Bnode<T>* result = search(root, key);
+
+    // if found return true else return false
+    return (result == NULL)? false: true;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @param subTreeRoot 
+ * @param key 
+ * @return Bnode<T>* 
+ */
+template<class T>
+Bnode<T>* BST<T>::Delete(Bnode<T>* subTreeRoot, T key)
+{
+    if (subTreeRoot == NULL)
+    {
+        // empty, return null
+        return NULL;
+    }
+    if (key < subTreeRoot->data)
+    {
+        // delete from left branch
+        subTreeRoot->left = Delete(subTreeRoot->left, key);
+    }
+    else if (key > subTreeRoot->data)
+    {
+        // delete from right branch
+        subTreeRoot->right = Delete(subTreeRoot->right, key);
+    }
+    else
+    {
+        if  (subTreeRoot->left == NULL && subTreeRoot->right == NULL)       // leave node
+        {
+            // remove any way
+            subTreeRoot = NULL;
+        }
+        else if (subTreeRoot->left != NULL && subTreeRoot->right == NULL)   // left exists and there is no right
+        {
+            subTreeRoot->data = subTreeRoot->left->data;
+            delete subTreeRoot->left;
+            subTreeRoot->left = NULL;
+        }
+        else if (subTreeRoot->left == NULL && subTreeRoot->right != NULL)   // right exists and there is no left
+        {
+            // store in child and remove that child
+            subTreeRoot->data = subTreeRoot->right->data;
+            delete subTreeRoot->right;
+            subTreeRoot->right = NULL;
+        }
+        else
+        {
+            // if node has two children
+            Bnode<T>* max = findMaxmum(subTreeRoot->left);
+            subTreeRoot->data =  max->data;
+
+            // delete max
+            subTreeRoot->left = Delete(subTreeRoot->left, max->data);
+        }
+
+    }
+
+    return subTreeRoot;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @param item 
+ */
+template <class T>
+void BST<T>::Delete(T item)
+{
+    // delete item based on root
+    root = Delete(root, item);
+}
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @param subTreeRoot 
+ * @return Bnode<T>* 
+ */
+template <class T>
+Bnode<T>* BST<T>::findMaxmum(Bnode<T>* subTreeRoot)
+{
+    /**
+     * note: max item in tree exists only in right branch
+     *
+     * if tree is empty, return
+     * else if left branch is empty, then root is the minimum
+     * else findmin in rest subTrees
+     */
+    if (subTreeRoot == NULL)
+    {
+        return NULL;
+    }
+    else if (subTreeRoot->right == NULL)
+    {
+        return subTreeRoot;
+    }
+    else 
+    {
+        return findMaxmum(subTreeRoot->right);
+    }
+}
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @param subTreeRoot 
+ * @return Bnode<T>* 
+ */
+template <class T>
+Bnode<T>* BST<T>::findMinimum(Bnode<T>* subTreeRoot)
+{
+    /**
+     * if tree is empty, return
+     * else if left branch is empty, then root is the minimum
+     * else findmin in rest subTrees
+     */
+    if (subTreeRoot == NULL)
+    {
+        return NULL;
+    }
+    else if (subTreeRoot->left == NULL)
+    {
+        return subTreeRoot;
+    }
+    else
+    {
+        return findMinimum(subTreeRoot->left);
+    }
+}
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @param subTreeRoot 
+ */
+template <class T>
+void BST<T>::preOrder(Bnode<T>* subTreeRoot)
+{
+    // if tree empty return without doing any things
+    if (subTreeRoot == NULL)
+    {
+        return; 
+    }
+    else
+    {
+        // root left right
+        cout << subTreeRoot->data << "\t";
+        preOrder(subTreeRoot->left);
+        preOrder(subTreeRoot->right);
+    }
+}
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ */
+template <class T>
 void BST<T>::preOrder()
 {
     preOrder(root);
 }
 
-template <typename T>
+template <class T>
+void BST<T>::inOrder(Bnode<T>* subTreeRoot)
+{
+    // if tree empty return without doing any things
+    if (subTreeRoot == NULL)
+    {
+        return; 
+    }
+    else
+    {
+        // left root right
+        inOrder(subTreeRoot->left);
+        cout << subTreeRoot->data << "\t";
+        inOrder(subTreeRoot->right);
+    }
+}
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ */
+template <class T>
 void BST<T>::inOrder()
 {
     inOrder(root);
 }
 
-template <typename T>
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ * @param subTreeRoot 
+ */
+template <class T>
+void BST<T>::postOrder(Bnode<T>* subTreeRoot)
+{
+    // if tree empty return without doing any things
+    if (subTreeRoot == NULL)
+    {
+        return; 
+    }
+    else
+    {
+        // left right root
+        postOrder(subTreeRoot->left);
+        postOrder(subTreeRoot->right);
+        cout << subTreeRoot->data << "\t";
+    }
+}
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ */
+template <class T>
 void BST<T>::postOrder()
 {
     postOrder(root);
